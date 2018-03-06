a6a145845f883d44c59fec6a22ad5a99efdf4235
Franck <franck.deniau@auxitec.fr>
2018-03-06 12:33:47 +0100

    Index includes initialization

---
+++ includes/inscription.inc.php
@@ -0,0 +1,3 @@
+<h1>
+    INSCRIPTION
+</h1>
--- index.php
+++ index.php
@@ -1,6 +1,16 @@
 <?php
 include "includes/header.php";
 
-echo "<h1>Ceci est les corps de mon index.php</h1>";
+echo "<h2>Ceci est les corps de mon index.php</h2>";
+
+if (isset($_GET['page']))
+    $page = $_GET['page'] != "" ? $_GET["page"] : "accueil";
+else
+    $page = "accueil";
+
+$file = "./includes/" . $page . ".inc.php";
+$files = glob("./includes/*.inc.php");
+
+if (in_array($file, $files)) include $file;
 
 include "includes/footer.php";
