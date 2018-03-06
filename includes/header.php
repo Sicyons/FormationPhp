7a23bee8e94cd83c0b934da62aa04ab9c57b3708
Franck <franck.deniau@auxitec.fr>
2018-03-06 12:13:46 +0100

    Index cleaning

--- includes/header.php
+++ includes/header.php
@@ -2,6 +2,6 @@
     <nav>
         <li><a href="index.php?page=accueil">Accueil</a></li>
         <li><a href="index.php?page=inscription">Inscription</a></li>
-        <li><a href="index.php?page=inscription">Inscription</a></li>
+        <li><a href="index.php?page=login">Login</a></li>
     </nav>
 </header>
