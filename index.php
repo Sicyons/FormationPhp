f0182de73437f50b114fa0c0a1357057dc109005
Franck <franck.deniau@auxitec.fr>
2018-03-06 11:46:26 +0100

    Index cleaning

--- index.php
+++ index.php
@@ -4,9 +4,3 @@ include "includes/header.php";
 echo "<h1>Ceci est les corps de mon index.php</h1>";
 
 include "includes/footer.php";
-/**
- * Created by PhpStorm.
- * User: Deniau
- * Date: 06/03/2018
- * Time: 10:43
- */
