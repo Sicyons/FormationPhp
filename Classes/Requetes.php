84ee7403dd91ee2e8266e5aad77430994fa344d2
Franck <franck.deniau@auxitec.fr>
2018-03-08 12:15:42 +0100

    Dbo initializations

---
+++ Classes/Requetes.php
@@ -0,0 +1,38 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Deniau
+ * Date: 08/03/2018
+ * Time: 11:34
+ */
+
+class Requetes
+{
+    private $dsn = "mysql:dbname=auxitec;host=localhost;charset=utf8";
+    private $user = "auxitec";
+    private $password = "Poseidon76*";
+    private $Db;
+
+    function __construct()
+    {
+        try {
+            $this->Db = new PDO($this->dsn, $this->$this->user, $this->password);
+            $this->Db->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_OBJ);
+        } catch (PDOException $e) {
+            Log::logWrite($e->getMessage());
+        }
+    }
+
+    function __destruct()
+    {
+        unset($this ->Db);
+    }
+
+    function insert($sql)
+    {
+        return $this->Db->exec($sql);
+    }
+    function select ($sql){
+        return $this->Db->query($sql);
+    }
+}
