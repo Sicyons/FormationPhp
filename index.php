1c76c54af746e4e3f7bffc3a0de3ce88f45b9529
Franck <franck.deniau@auxitec.fr>
2018-03-13 13:18:31 +0100

    Project creation

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8"/>
+        <meta title="Planning Manager"/>
+        <link href="content/css/bootstrap.min.css" rel="stylesheet" type="text/css">
+        <script src="content/javascript/jquery-3.3.1.min.js" type="text/javascript"></script>
+        <script type="text/javascript" src="content/javascript/popper.min.js"></script>
+        <script type="text/javascript" src="content/javascript/bootstrap.min.js"></script>
+    </head>
+    <body>
+    </body>
+</html>
